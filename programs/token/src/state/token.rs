b28d42d886c2fc340d9b13c96a9099cc458c1fea
Dean 利迪恩 <10921578+deanmlittle@users.noreply.github.com>
2024-10-27 15:06:30 +0800

    Update token.rs

--- programs/token/src/state/token.rs
+++ programs/token/src/state/token.rs
@@ -10,6 +10,8 @@ pub struct Token {
 impl TokenAccount {
     pub const LEN: usize = 165;
 
+    /// Use this when you know the AccountInfo will have the correct owner and account space
+    #[inline(always)]
     pub fn from_account_info_unchecked(account_info: &AccountInfo) -> Self {
         unsafe { Self(account_info.borrow_data_unchecked().as_ptr()) }
     }
@@ -17,7 +19,7 @@ impl TokenAccount {
     pub fn from_account_info(account_info: &AccountInfo) -> Self {
         assert_eq!(account_info.data_len(), Self::LEN);
         assert_eq!(account_info.owner(), &crate::ID);
-        unsafe { Self(account_info.borrow_data_unchecked().as_ptr()) }
+        Self::from_account_info_unchecked(account_info)
     }
 
     pub fn mint(&self) -> Pubkey {
@@ -32,21 +34,25 @@ impl TokenAccount {
         unsafe { *(self.0.add(64) as *const u64) }
     }
 
+    #[inline(always)]
     pub fn has_delegate(&self) -> bool {
         unsafe { *(self.0.add(72) as *const bool) }
     }
 
-    pub fn delegate(&self) -> Pubkey {
-        unsafe { *(self.0.add(76) as *const Pubkey) }
-    }
-
-    pub fn optional_delegate(&self) -> Option<Pubkey> {
+    pub fn delegate(&self) -> Option<Pubkey> {
         if self.has_delegate() {
-            Some(self.delegate())
+            Some(self.delegate_unchecked())
         } else {
             None
         }
     }
+
+    /// Use this when you know the account will have a delegate and want to skip the Option check.
+    #[inline(always)]
+    pub fn delegate_unchecked(&self) -> Pubkey {
+        unsafe { *(self.0.add(76) as *const Pubkey) }
+    }
+
 
     pub fn state(&self) -> AccountState {
         unsafe { *(self.0.add(108) as *const AccountState) }
@@ -56,35 +62,40 @@ impl TokenAccount {
         unsafe { *(self.0.add(109) as *const bool) }
     }
 
-    pub fn native_amount(&self) -> u64 {
-        unsafe { *(self.0.add(113) as *const u64) }
-    }
-
-    pub fn optional_native_amount(&self) -> Option<u64> {
+    pub fn native_amount(&self) -> Option<u64> {
         if self.is_native() {
-            Some(self.native_amount())
+            Some(self.native_amount_unchecked())
         } else {
             None
         }
     }
 
+    /// Use this when you know the account is native and you want to skip the Option check.
+    #[inline(always)]
+    pub fn native_amount_unchecked(&self) -> u64 {
+        unsafe { *(self.0.add(113) as *const u64) }
+    }
+    
     pub fn delegated_amount(&self) -> u64 {
         unsafe { *(self.0.add(121) as *const u64) }
     }
 
+    #[inline(always)]
     pub fn has_close_authority(&self) -> bool {
         unsafe { *(self.0.add(129) as *const bool) }
     }
 
-    pub fn close_authority(&self) -> Pubkey {
-        unsafe { *(self.0.add(133) as *const Pubkey) }
-    }
-
-    pub fn optional_close_authority(&self) -> Option<Pubkey> {
+    pub fn close_authority(&self) -> Option<Pubkey> {
         if self.has_close_authority() {
-            Some(self.close_authority())
+            Some(self.close_authority_unchecked())
         } else {
             None
         }
+    }
+
+    /// Use this when you know the account will a close authority and you want to skip the Option check.
+    #[inline(always)]
+    pub fn close_authority_unchecked(&self) -> Pubkey {
+        unsafe { *(self.0.add(133) as *const Pubkey) }
     }
 }
