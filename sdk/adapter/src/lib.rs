1c350fa31d2944799f344c2b9bd5d28897eaec1b
febo <febo@anza.xyz>
2025-01-27 14:08:01 +0000

    Add to_program_error helper

--- sdk/adapter/src/lib.rs
+++ sdk/adapter/src/lib.rs
@@ -3,3 +3,12 @@
 #![no_std]
 
 pub mod account_info;
+
+/// Converts a `pinocchio::program_error::ProgramError` to a
+/// `solana_program_error::ProgramError`.
+#[macro_export]
+macro_rules! to_program_error {
+    ($error:expr) => {
+        solana_program_error::ProgramError::from(u64::from($error))
+    };
+}
